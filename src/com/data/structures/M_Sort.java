c1c2e11103482c9d6fee6278b77621ed71cdc6d7
azimshaik <cheer.buddu@gmail.com>
2016-12-27 18:19:33 -0500

    merge helper function

--- src/com/data/structures/M_Sort.java
+++ src/com/data/structures/M_Sort.java
@@ -1,10 +1,13 @@
 package com.data.structures;
 
 public class M_Sort {
-	
+	public static void merge_sort(int[] a){
+		
+	}
 	
 	public static void main(String[] args){
-		
+		int[] a = {12, 34};
+		merge_sort(a);
 	}
 
 }
