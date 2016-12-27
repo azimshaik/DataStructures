c166ce69701f9f9ac8f50b8f826ee5333f6afc15
azimshaik <cheer.buddu@gmail.com>
2016-12-27 18:24:29 -0500

    recusrive call edge condition check

--- src/com/data/structures/M_Sort.java
+++ src/com/data/structures/M_Sort.java
@@ -5,7 +5,11 @@ public class M_Sort {
 		//merge_sort recusrive method def 
 		merge_sort(a,0,a.length-1);
 	}
-	
+	public static void merge_sort(int[] a, int low, int high){
+		if(low>=high){//edge condition
+			return;
+		}
+	}
 	public static void main(String[] args){
 		int[] a = {12, 34};
 		merge_sort(a);
