4c0061541703a7b65f598dc746a357a9c243dcaa
azimshaik <cheer.buddu@gmail.com>
2016-12-27 18:59:42 -0500

    Merge_Sort Hell Yeahhgit add *

--- src/com/data/structures/M_Sort.java
+++ src/com/data/structures/M_Sort.java
@@ -50,8 +50,11 @@ public class M_Sort {
 		}
 	}
 	public static void main(String[] args){
-		int[] a = {12, 34};
+		int[] a = {12, 34,0,-9,100};
 		merge_sort(a);
+		for(int i = 0;i<a.length;i++){
+			System.out.print(a[i]+",");
+		}
 	}
 
 }
