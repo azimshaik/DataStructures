88aabaeb9bfee73b7c13fa0ccc7aeb5cdfb6cd6e
azimshaik <cheer.buddu@gmail.com>
2017-01-24 19:49:34 -0500

    Left rotation, almost close

---
+++ src/com/data/structures/LeftRotation.java
@@ -0,0 +1,31 @@
+package com.data.structures;
+import java.util.Scanner;
+public class LeftRotation {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int d = sc.nextInt();
+		int[] A = new int[n];
+		//System.out.println(n+"_"+d);
+		for(int i=0;i<n;i++){
+			A[i] = sc.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			System.out.print(A[i]+" ");
+		}
+		for(int i=0;i<d;i++){
+			int m =n;
+			int j =i;
+			int temp = A[i];
+			while(m>1){
+				A[j-i] = A[j-i+1];
+				j++;
+				m--;
+			}
+			A[n-1] = temp;
+		}
+		for(int i=0;i<n;i++)
+			System.out.println(A[i]+" ");
+	}
+}
