fbcf19a3bce2bab35c503474b2e58a36693d0f27
azimshaik <cheer.buddu@gmail.com>
2017-01-28 23:59:21 -0500

    String matching with Naive approach

--- src/com/data/structures/StringMatching.java
+++ src/com/data/structures/StringMatching.java
@@ -1,5 +1,14 @@
 package com.data.structures;
-
+import java.util.Scanner;
 public class StringMatching {
-	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String ss = sc.next();
+		int s_len = s.length();
+		int ss_len = ss.length();
+		System.out.println(s_len+" "+ss_len);
+		//helloworldjava
+		//world
+	}
 }
