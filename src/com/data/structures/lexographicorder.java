fa04ed5a9c8d4fdea78209db69a52076c685b764
azimshaik <cheer.buddu@gmail.com>
2017-01-28 23:12:53 -0500

    Working Lexical order

--- src/com/data/structures/lexographicorder.java
+++ src/com/data/structures/lexographicorder.java
@@ -1,5 +1,22 @@
 package com.data.structures;
-
+import java.util.Scanner;
 public class lexographicorder {
-
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
+		int len = s1.length()+s2.length();
+		System.out.println(len);
+		int x = s1.compareTo(s2);
+		String status;
+		if(x<0){
+			status = "Yes";
+		}else if(x==0){
+			status = "Equal";
+		}else{
+			status = "No";
+		}
+		System.out.println(status);
+		System.out.println(s1.substring(0, 1).toUpperCase() + s1.substring(1)+" "+s2.substring(0,1).toUpperCase()+s2.substring(1));
+	}
 }
