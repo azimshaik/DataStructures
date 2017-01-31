3298cb5a55c18727b88a9f27ccab459308b2eb5a
azimshaik <cheer.buddu@gmail.com>
2017-01-31 06:10:32 -0500

    String Matching with Substring and equals method

--- src/com/data/structures/StringMatching.java
+++ src/com/data/structures/StringMatching.java
@@ -4,18 +4,17 @@ import java.util.Scanner;
 public class StringMatching {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		String ss = sc.next();
-		int s_len = s.length();
-		int ss_len = ss.length();
-		System.out.println(s_len+" "+ss_len);
+		String text = sc.next();
+		String pat = sc.next();
+		int n = text.length();
+		int m = pat.length();
 		//helloworldjavaworld
 		//world
-		for(int i=0; i<ss_len;i++){
-			for(int j=0;j<s_len-ss_len;j++){
-				if(ss.toCharArray()[i] == s.toCharArray()[j]){
-					i++;
-				}
+		for(int i=0; i<=n-m; i++){
+			System.out.println(text.substring(i,i+m));
+			if(pat.equals(text.substring(i,i+m))){
+				System.out.println("Match found at"+i);
+			
 			}
 		}
 	}
